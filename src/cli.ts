c5d6da2cf32e124dc851655c9e9645b9a062c927
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-05-03 00:10:23 +0900

    fix execCommand usage

--- src/cli.ts
+++ src/cli.ts
@@ -6,13 +6,13 @@ program
 
 program
     .description("VRChat join notifier")
-    .option("-s, --specific-names <name...>", "specific notification names")
-    .option("-se, --specific-exec <command>", "exec command when match specific names")
+    .option("-s, --specific-names <name...>", "specific notification names(with another notification sound)")
+    .option("-se, --specific-exec <command>", "exec command when match specific names. Replace %{{names}} in command text with join user names")
     .option("-i, --interval <sec>", "specify check interval", "2")
 
 export async function run(argv: any): Promise<void> {
     program.parse(argv);
-    console.log("notifier running...");
+    console.log("notifier running...", program["specificExec"]);
     app({
         interval: program["interval"],
         specificNames: program["specificNames"],
