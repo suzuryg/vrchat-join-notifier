0885fb3fb2b9a29b199d8d7ec9623e441dc29b3f
kamakiri01 <kamakiri01@users.noreply.github.com>
2021-05-05 23:05:43 +0900

    fix config read

--- src/cli.ts
+++ src/cli.ts
@@ -13,12 +13,17 @@ program
     .option("-s, --specific-names <name...>", "specific notification names(with another notification sound)")
     .option("-se, --specific-exec <command>", "exec command when match specific names. Replace %{{names}} in command text with join user names")
     .option("-i, --interval <sec>", "specify check interval", "2")
+    .option("-nt, --no-toast", "prevent toast notification")
+    .option("-nx, --no-xsoverlay", "prevent xsoverlay notification")
 
 export async function run(argv: any): Promise<void> {
     program.parse(argv);
+
     app({
         interval: program["interval"],
         specificNames: program["specificNames"],
-        specificExec: program["specificExec"]
+        specificExec: program["specificExec"],
+        isToast: program["toast"],
+        isXSOverlay: program["xsoverlay"]
     });
 }
